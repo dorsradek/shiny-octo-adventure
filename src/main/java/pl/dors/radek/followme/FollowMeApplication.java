6ec5e970c1a225d5e4399eb1fc58550306f8de25
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-21 12:12:37 +0200

    test data access

--- src/main/java/pl/dors/radek/followme/FollowMeApplication.java
+++ src/main/java/pl/dors/radek/followme/FollowMeApplication.java
@@ -16,4 +16,34 @@ public class FollowMeApplication {
         SpringApplication.run(FollowMeApplication.class, args);
     }
 
+    @Component
+    class CLR implements CommandLineRunner {
+
+        @Autowired
+        private CustomerRepository repository;
+
+        @Override
+        public void run(String... args) throws Exception {
+            repository.deleteAll();
+
+            // save a couple of customers
+            repository.save(new Customer("Alice", "Smith"));
+            repository.save(new Customer("Bob", "Smith"));
+
+            // fetch all customers
+            System.out.println("Customers found with findAll():");
+            System.out.println("-------------------------------");
+            repository.findAll().forEach(System.out::println);
+            System.out.println();
+
+            // fetch an individual customer
+            System.out.println("Customer found with findByFirstName('Alice'):");
+            System.out.println("--------------------------------");
+            System.out.println(repository.findByFirstName("Alice"));
+
+            System.out.println("Customers found with findByLastName('Smith'):");
+            System.out.println("--------------------------------");
+            repository.findByLastName("Smith").forEach(System.out::println);
+        }
+    }
 }
