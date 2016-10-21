92626492f777fd64c28060d959cad3cdc9f7a727
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-21 15:49:04 +0200

    simple save fetch place meeting

--- src/main/java/pl/dors/radek/followme/FollowMeApplication.java
+++ src/main/java/pl/dors/radek/followme/FollowMeApplication.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
 import org.springframework.stereotype.Component;
+import pl.dors.radek.followme.model.Customer;
+import pl.dors.radek.followme.model.Meeting;
+import pl.dors.radek.followme.model.Place;
+import pl.dors.radek.followme.repository.CustomerRepository;
+import pl.dors.radek.followme.repository.MeetingRepository;
+import pl.dors.radek.followme.repository.PlaceRepository;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {EmbeddedMongoAutoConfiguration.class})
@@ -20,30 +29,74 @@ public class FollowMeApplication {
     class CLR implements CommandLineRunner {
 
         @Autowired
-        private CustomerRepository repository;
+        private CustomerRepository customerRepository;
+
+        @Autowired
+        private PlaceRepository placeRepository;
+
+        @Autowired
+        private MeetingRepository meetingRepository;
 
         @Override
         public void run(String... args) throws Exception {
-            repository.deleteAll();
+            prepareCustomers();
+            preparePlaces();
+            prepareMeetings();
+        }
+
+        private void prepareCustomers() {
+            customerRepository.deleteAll();
 
             // save a couple of customers
-            repository.save(new Customer("Alice", "Smith"));
-            repository.save(new Customer("Bob", "Smith"));
+            customerRepository.save(new Customer("Alice", "Smith"));
+            customerRepository.save(new Customer("Bob", "Smith"));
 
             // fetch all customers
             System.out.println("Customers found with findAll():");
             System.out.println("-------------------------------");
-            repository.findAll().forEach(System.out::println);
+            customerRepository.findAll().forEach(System.out::println);
             System.out.println();
 
             // fetch an individual customer
             System.out.println("Customer found with findByFirstName('Alice'):");
             System.out.println("--------------------------------");
-            System.out.println(repository.findByFirstName("Alice"));
+            System.out.println(customerRepository.findByFirstName("Alice"));
 
             System.out.println("Customers found with findByLastName('Smith'):");
             System.out.println("--------------------------------");
-            repository.findByLastName("Smith").forEach(System.out::println);
+            customerRepository.findByLastName("Smith").forEach(System.out::println);
+        }
+
+        private void preparePlaces() {
+            placeRepository.deleteAll();
+
+            placeRepository.save(new Place(1, 1.0, "Place 1"));
+            placeRepository.save(new Place(2, 3.0, "Place 2"));
+
+            // fetch all places
+            System.out.println("Places found with findAll():");
+            System.out.println("-------------------------------");
+            placeRepository.findAll().forEach(System.out::println);
+        }
+
+        private void prepareMeetings() {
+            List<Place> places = Arrays.asList(new Place(1, 1.0, "Place Meeting 1"), new Place(2, 3.0, "Place Meeting 2"));
+            places.stream().forEach(placeRepository::save);
+
+            meetingRepository.deleteAll();
+
+            Meeting m1 = new Meeting(places);
+            meetingRepository.save(m1);
+            Meeting m2 = new Meeting(places);
+            meetingRepository.save(m2);
+
+            // fetch all meetings
+            System.out.println("-------------------------------");
+            System.out.println("Meetings found with findAll():");
+            System.out.println("-------------------------------");
+            meetingRepository.findAll().stream()
+                    .flatMap(m -> m.getPlaces().stream())
+                    .forEach(System.out::println);
         }
     }
 }
