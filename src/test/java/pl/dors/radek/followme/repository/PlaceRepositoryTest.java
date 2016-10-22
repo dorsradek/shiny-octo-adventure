879448e2367dcb13be806026eb3a06eee4f85111
rdors <dors.radek@gmail.com>
2016-10-23 00:44:49 +0200

    PlaceRepositoryTest

---
+++ src/test/java/pl/dors/radek/followme/repository/PlaceRepositoryTest.java
@@ -0,0 +1,45 @@
+package pl.dors.radek.followme.repository;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.dors.radek.followme.model.Place;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableAutoConfiguration
+public class PlaceRepositoryTest {
+
+    @Autowired
+    private MongoTemplate template;
+
+    @Autowired
+    private PlaceRepository placeRepository;
+
+    private Place place;
+
+    @Before
+    public void setUp() throws Exception {
+        place = new Place(new GeoJsonPoint(12, 13), "Stefan");
+        template.save(place);
+    }
+
+    @Test
+    public void findAllTest() {
+        List<Place> places = placeRepository.findAll();
+        assertThat(places).hasSize(1);
+        assertThat(places).containsOnly(place);
+    }
+
+}
