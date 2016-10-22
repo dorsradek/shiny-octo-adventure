a12386ecdbd98c078efb631aa4b2aea9c9321c6f
rdors <dors.radek@gmail.com>
2016-10-23 00:45:08 +0200

    PlaceServiceTest using spring mockito

---
+++ src/test/java/pl/dors/radek/followme/service/PlaceServiceTest.java
@@ -0,0 +1,39 @@
+package pl.dors.radek.followme.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
+import org.springframework.test.context.junit4.SpringRunner;
+import pl.dors.radek.followme.model.Place;
+import pl.dors.radek.followme.repository.PlaceRepository;
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
+
+/**
+ * Created by rdors on 2016-10-22.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class PlaceServiceTest {
+
+    @Autowired
+    private PlaceService placeService;
+
+    @MockBean(name = "placeRepository")
+    private PlaceRepository placeRepository;
+
+    @Test
+    public void findAllTest() {
+        given(this.placeRepository.findAll()).willReturn(Arrays.asList(new Place(new GeoJsonPoint(12, 13), "Stefan")));
+        Stream<Place> places = placeService.findAll();
+        assertThat(places.peek(System.out::println).collect(Collectors.toList())).hasSize(1);
+    }
+}
