e03f72efc4b578c0168b7a8745620b532fe84ffe
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-24 11:50:52 +0200

    added PlaceRepositoryCustom interface

--- src/main/java/pl/dors/radek/followme/repository/PlaceRepository.java
+++ src/main/java/pl/dors/radek/followme/repository/PlaceRepository.java
@@ -1,11 +1,14 @@
 package pl.dors.radek.followme.repository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 import pl.dors.radek.followme.model.Place;
+import pl.dors.radek.followme.repository.custom.PlaceRepositoryCustom;
 
 /**
  * Created by rdors on 2016-10-21.
  */
-public interface PlaceRepository extends MongoRepository<Place, Long> {
+@Repository
+public interface PlaceRepository extends MongoRepository<Place, Long>, PlaceRepositoryCustom {
 
 }
---
+++ src/main/java/pl/dors/radek/followme/repository/custom/PlaceRepositoryCustom.java
@@ -0,0 +1,14 @@
+package pl.dors.radek.followme.repository.custom;
+
+import pl.dors.radek.followme.model.Place;
+
+import java.util.List;
+
+/**
+ * Created by rdors on 2016-10-24.
+ */
+public interface PlaceRepositoryCustom {
+
+    List<Place> findByNameCriteria(String name);
+
+}
