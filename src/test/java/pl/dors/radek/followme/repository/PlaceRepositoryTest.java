5cdcc3e6dbe246928eb012a23b4f00b17d35c215
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-24 11:51:07 +0200

    new test for PlaceRepositoryTest

--- src/test/java/pl/dors/radek/followme/repository/PlaceRepositoryTest.java
+++ src/test/java/pl/dors/radek/followme/repository/PlaceRepositoryTest.java
@@ -1,5 +1,6 @@
 package pl.dors.radek.followme.repository;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -11,7 +12,10 @@ import org.springframework.data.mongodb.core.geo.GeoJsonPoint;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.dors.radek.followme.model.Place;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,20 +31,35 @@ public class PlaceRepositoryTest {
     @Autowired
     private PlaceRepository placeRepository;
 
-    private Place place;
+    private List<Place> places;
 
     @Before
     public void setUp() throws Exception {
-        place = new Place(new GeoJsonPoint(12, 13), "Stefan");
-        template.save(place);
+        places = Arrays.asList(
+                new Place(new GeoJsonPoint(12, 13), "Stefan"),
+                new Place(new GeoJsonPoint(11, 11), "ASD")
+        );
+        places.forEach(template::save);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        template.dropCollection(Place.class);
     }
 
     @Test
-    public void findAllTest() {
+    public void findAllTest() throws Exception {
         List<Place> result = placeRepository.findAll();
 
-        assertThat(result).hasSize(1);
-        assertThat(result).containsOnly(place);
+        assertThat(result).hasSize(2);
+        assertThat(result).containsAll(places);
     }
 
+    @Test
+    public void findByNameCriteria() throws Exception {
+        List<Place> result = placeRepository.findByNameCriteria("Stefan");
+
+        assertThat(result).hasSize(1);
+        assertThat(result).containsOnly(places.stream().filter(p -> p.getName().equals("Stefan")).findAny().get());
+    }
 }
