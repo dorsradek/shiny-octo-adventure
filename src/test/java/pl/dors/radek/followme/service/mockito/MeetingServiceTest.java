59d0e93caf775d05168b9576150bd19cab430abb
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-26 15:39:15 +0200

    meeting save test

--- src/test/java/pl/dors/radek/followme/service/mockito/MeetingServiceTest.java
+++ src/test/java/pl/dors/radek/followme/service/mockito/MeetingServiceTest.java
@@ -6,19 +6,17 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.data.domain.Example;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.test.context.junit4.SpringRunner;
 import pl.dors.radek.followme.model.Meeting;
 import pl.dors.radek.followme.model.Place;
+import pl.dors.radek.followme.model.User;
 import pl.dors.radek.followme.repository.MeetingRepository;
 import pl.dors.radek.followme.repository.PlaceRepository;
 import pl.dors.radek.followme.repository.UserRepository;
 import pl.dors.radek.followme.service.MeetingService;
-import pl.dors.radek.followme.service.PlaceService;
 
 import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Matchers.any;
@@ -39,14 +37,30 @@ public class MeetingServiceTest {
     @Mock
     private UserRepository userRepository;
 
+    private static long id;
+
     @Before
     public void setUp() throws Exception {
         Mockito.when(this.meetingRepository.findAll())
                 .thenReturn(Arrays.asList(new Meeting("M1")));
-        Mockito.when(this.placeRepository.findAll(any(Example.class)))
-                .thenReturn(Arrays.asList(new Place("Place2", 10, 9)));
-        Mockito.when(this.placeRepository.findAll(any(Specification.class)))
-                .thenReturn(Arrays.asList(new Place("Place3", 11, 15)));
+        Mockito.when(this.placeRepository.save(any(Place.class)))
+                .then(i -> {
+                    Place place = i.getArgumentAt(0, Place.class);
+                    place.setId(id++);
+                    return i;
+                });
+        Mockito.when(this.userRepository.save(any(User.class)))
+                .then(i -> {
+                    User user = i.getArgumentAt(0, User.class);
+                    user.setId(id++);
+                    return i;
+                });
+        Mockito.when(this.meetingRepository.save(any(Meeting.class)))
+                .then(i -> {
+                    Meeting meeting = i.getArgumentAt(0, Meeting.class);
+                    meeting.setId(id++);
+                    return i;
+                });
     }
 
     @Test
@@ -57,7 +71,18 @@ public class MeetingServiceTest {
 
     @Test
     public void save() throws Exception {
-
+        Meeting meeting = new Meeting("M1");
+        User user = new User("U1");
+        Place place = new Place("P1", 1, 2);
+        List<User> users = Arrays.asList(user);
+        List<Place> places = Arrays.asList(place);
+        meetingService.save(meeting, users, places);
+        assertThat(meeting.getMeetingUsers()).hasSize(1);
+        assertThat(meeting.getMeetingPlaces()).hasSize(1);
+        assertThat(meeting.getMeetingUsers().get(0).getUser().getName()).isEqualTo("U1");
+        assertThat(meeting.getMeetingPlaces().get(0).getPlace().getName()).isEqualTo("P1");
+        assertThat(meeting.getMeetingUsers().get(0).getUser().getId()).isNotNull();
+        assertThat(meeting.getMeetingPlaces().get(0).getPlace().getId()).isNotNull();
     }
 
     @Test
