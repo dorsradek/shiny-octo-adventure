e407f21c867f059ba0aaf612bd7beec740b64e46
rdors <dors.radek@gmail.com>
2016-10-26 23:44:07 +0200

    MeetingPlaceSpecification

--- src/main/java/pl/dors/radek/followme/specification/MeetingPlaceSpecification.java
+++ src/main/java/pl/dors/radek/followme/specification/MeetingPlaceSpecification.java
@@ -3,6 +3,7 @@ package pl.dors.radek.followme.specification;
 import org.springframework.data.jpa.domain.Specification;
 import pl.dors.radek.followme.model.Meeting;
 import pl.dors.radek.followme.model.MeetingPlace;
+import pl.dors.radek.followme.model.Place;
 
 /**
  * Created by rdors on 2016-10-25.
@@ -11,5 +12,9 @@ public class MeetingPlaceSpecification {
 
     public static Specification<MeetingPlace> findByMeeting(Meeting meeting) {
         return (root, query, cb) -> cb.equal(root.get("pk").get("meeting").get("id"), meeting.getId());
+    }
+
+    public static Specification<MeetingPlace> findByPlace(Place place) {
+        return (root, query, cb) -> cb.equal(root.get("pk").get("place").get("id"), place.getId());
     }
 }
