7b1aca4c20a9717d4101489c40f733ff6618b04e
rdors <dors.radek@gmail.com>
2016-10-26 20:47:43 +0200

    test refactor

---
+++ src/test/java/pl/dors/radek/followme/service/MeetingServiceCommon.java
@@ -0,0 +1,73 @@
+package pl.dors.radek.followme.service;
+
+import org.mockito.Mockito;
+import pl.dors.radek.followme.model.Meeting;
+import pl.dors.radek.followme.model.Place;
+import pl.dors.radek.followme.model.User;
+import pl.dors.radek.followme.repository.MeetingRepository;
+import pl.dors.radek.followme.repository.PlaceRepository;
+import pl.dors.radek.followme.repository.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+
+/**
+ * Created by rdors on 2016-10-26.
+ */
+public class MeetingServiceCommon {
+
+    private static long id;
+
+    public static void setUp(MeetingRepository meetingRepository, UserRepository userRepository, PlaceRepository placeRepository) throws Exception {
+        Mockito.when(meetingRepository.findAll())
+                .thenReturn(Arrays.asList(new Meeting("M1")));
+        Mockito.when(placeRepository.save(any(Place.class)))
+                .then(i -> {
+                    Place place = i.getArgumentAt(0, Place.class);
+                    place.setId(id++);
+                    return i;
+                });
+        Mockito.when(userRepository.save(any(User.class)))
+                .then(i -> {
+                    User user = i.getArgumentAt(0, User.class);
+                    user.setId(id++);
+                    return i;
+                });
+        Mockito.when(meetingRepository.save(any(Meeting.class)))
+                .then(i -> {
+                    Meeting meeting = i.getArgumentAt(0, Meeting.class);
+                    meeting.setId(id++);
+                    return i;
+                });
+    }
+
+    public static void findAll(MeetingService meetingService) throws Exception {
+        assertThat(meetingService.findAll()).hasSize(1);
+        assertThat(meetingService.findAll().get(0).getName()).isEqualTo("M1");
+    }
+
+    public static void save(MeetingService meetingService) throws Exception {
+        Meeting meeting = new Meeting("M1");
+        User user = new User("U1");
+        Place place = new Place("P1", 1, 2);
+        List<User> users = Arrays.asList(user);
+        List<Place> places = Arrays.asList(place);
+        meetingService.save(meeting, users, places);
+        assertThat(meeting.getMeetingUsers()).hasSize(1);
+        assertThat(meeting.getMeetingPlaces()).hasSize(1);
+        assertThat(meeting.getMeetingUsers().get(0).getUser().getName()).isEqualTo("U1");
+        assertThat(meeting.getMeetingPlaces().get(0).getPlace().getName()).isEqualTo("P1");
+        assertThat(meeting.getMeetingUsers().get(0).getUser().getId()).isNotNull();
+        assertThat(meeting.getMeetingPlaces().get(0).getPlace().getId()).isNotNull();
+    }
+
+    public static void addPlaces(MeetingService meetingService) {
+    }
+
+    public static void addUsers(MeetingService meetingService) {
+
+    }
+}
