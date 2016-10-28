e8acafd46432507424fcbd38c124a54b481b5cc3
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-28 14:42:16 +0200

    delete user specification

--- src/main/java/pl/dors/radek/followme/repository/MeetingPlaceRepository.java
+++ src/main/java/pl/dors/radek/followme/repository/MeetingPlaceRepository.java
@@ -1,11 +1,20 @@
 package pl.dors.radek.followme.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import pl.dors.radek.followme.model.MeetingPlace;
+
+import java.util.List;
 
 /**
  * Created by rdors on 2016-10-25.
  */
-public interface MeetingPlaceRepository extends JpaRepository<MeetingPlace, Long>, JpaSpecificationExecutor<MeetingPlace> {
+public interface MeetingPlaceRepository extends CrudRepository<MeetingPlace, Long> {
+
+    List<MeetingPlace> findAll();
+
+    @Query(value = "SELECT mu FROM MeetingPlace mu WHERE mu.pk.meeting.id = :meetingId")
+    List<MeetingPlace> findByMeetingId(@Param("meetingId") Long meetingId);
+
 }
