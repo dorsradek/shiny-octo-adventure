8abadae5334a09a3c06b20e40540791f503debcd
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-10-28 14:06:22 +0200

    remove of specifications

--- src/main/java/pl/dors/radek/followme/specification/UserSpecification.java
+++ src/main/java/pl/dors/radek/followme/specification/UserSpecification.java
@@ -3,9 +3,15 @@ package pl.dors.radek.followme.specification;
 import org.springframework.data.jpa.domain.Specification;
 import pl.dors.radek.followme.model.User;
 
+import java.util.Optional;
+
 public class UserSpecification {
 
     public static Specification<User> findByName(String name) {
         return (root, query, cb) -> cb.equal(root.get("name"), name);
+    }
+
+    public static Specification<User> findOne(Optional<Long> id) {
+        return (root, query, cb) -> cb.equal(root.get("id"), id.orElseThrow(() -> new IllegalArgumentException("User id can't be null")));
     }
 }
