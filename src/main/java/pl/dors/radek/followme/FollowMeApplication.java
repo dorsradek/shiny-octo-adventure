bc754d9cecfcdf704a4ca70e9f812288d28ac3bd
rdors <dors.radek@gmail.com>
2016-11-09 23:21:27 +0100

    prod fix

--- src/main/java/pl/dors/radek/followme/FollowMeApplication.java
+++ src/main/java/pl/dors/radek/followme/FollowMeApplication.java
@@ -1,22 +1,7 @@
 package pl.dors.radek.followme;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-import pl.dors.radek.followme.model.security.Authority;
-import pl.dors.radek.followme.model.security.AuthorityName;
-import pl.dors.radek.followme.model.security.User;
-import pl.dors.radek.followme.security.repository.AuthorityRepository;
-import pl.dors.radek.followme.security.repository.UserRepository;
-
-import java.time.Instant;
-import java.time.LocalDateTime;
-import java.time.ZoneOffset;
-import java.util.Arrays;
-import java.util.Date;
 
 @SpringBootApplication
 public class FollowMeApplication {
@@ -25,37 +10,4 @@ public class FollowMeApplication {
         SpringApplication.run(FollowMeApplication.class, args);
     }
 
-}
-
-@Component
-@Profile("!test")
-class JobCommandLineRunner implements CommandLineRunner {
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private AuthorityRepository authorityRepository;
-
-    @Override
-    public void run(String... args) throws Exception {
-        Authority a = new Authority();
-        a.setName(AuthorityName.ROLE_ADMIN);
-        authorityRepository.save(a);
-
-        User user = new User();
-        user.setUsername("admin");
-        user.setPassword("$2a$08$lDnHPz7eUkSi6ao14Twuau08mzhWrL4kyZGGU5xfiGALO/Vxd5DOi");
-        user.setFirstname("admin");
-        user.setLastname("admin");
-        user.setEmail("Asdfgh");
-        user.setEnabled(true);
-        LocalDateTime dateTime = LocalDateTime.now().minusDays(5);
-        Instant instant = dateTime.toInstant(ZoneOffset.UTC);
-        user.setLastPasswordResetDate(Date.from(instant));
-        user.setAuthorities(Arrays.asList(a));
-        userRepository.save(user);
-
-        userRepository.findAll().forEach(System.out::println);
-    }
 }
--- src/test/java/pl/dors/radek/followme/controller/PlaceControllerTest.java
+++ src/test/java/pl/dors/radek/followme/controller/PlaceControllerTest.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -31,7 +30,6 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
  */
 @RunWith(SpringRunner.class)
 @WebMvcTest(PlaceController.class)
-@ActiveProfiles("test")
 public class PlaceControllerTest {
 
     @Autowired
