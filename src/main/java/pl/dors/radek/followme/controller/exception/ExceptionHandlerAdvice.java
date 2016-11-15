388e415b1ff2bec0517625b16b7ebfbab11e45a6
rdors <dors.radek@gmail.com>
2016-11-15 20:44:24 +0100

    logging exceptions

--- src/main/java/pl/dors/radek/followme/controller/exception/ExceptionHandlerAdvice.java
+++ src/main/java/pl/dors/radek/followme/controller/exception/ExceptionHandlerAdvice.java
@@ -1,20 +1,25 @@
 package pl.dors.radek.followme.controller.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import pl.dors.radek.followme.security.config.WebSecurityConfigProduction;
 
 @ControllerAdvice
 public class ExceptionHandlerAdvice {
 
     private static final HttpStatus HTTP_STATUS = HttpStatus.BAD_REQUEST;
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExceptionHandlerAdvice.class);
+
     @ExceptionHandler(Exception.class)
     public ResponseEntity handleException(Exception e) {
         ErrorResponse error = new ErrorResponse();
         error.setErrorCode(HTTP_STATUS.value());
-        System.out.println(e.getMessage());
+        LOGGER.debug(e.getMessage());
         error.setMessage("Error");
         return new ResponseEntity<>(error, HTTP_STATUS);
     }
