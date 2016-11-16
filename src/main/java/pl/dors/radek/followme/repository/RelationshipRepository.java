adaa6200c93da4bcac661dfc3735965aa4b5162b
rdors <dors.radek@gmail.com>
2016-11-16 16:02:56 +0100

    relationshipRepository

---
+++ src/main/java/pl/dors/radek/followme/repository/RelationshipRepository.java
@@ -0,0 +1,36 @@
+package pl.dors.radek.followme.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import pl.dors.radek.followme.model.Relationship;
+import pl.dors.radek.followme.model.RelationshipStatus;
+
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * Created by stephan on 20.03.16.
+ */
+public interface RelationshipRepository extends CrudRepository<Relationship, Long> {
+
+    List<Relationship> findAll();
+
+    @Query("SELECT r FROM Relationship r WHERE r.pk.user.id = :userId AND r.relationshipStatus = pl.dors.radek.followme.model.RelationshipStatus.ACCEPTED")
+    List<Relationship> findAllFriendsByUserId(@Param("userId") Long userId);
+
+    @Query("SELECT r FROM Relationship r WHERE r.pk.user.id = :userId AND r.relationshipStatus = :status")
+    List<Relationship> findAllRelationshipsByOwnerUserIdAndStatus(@Param("userId") Long userId, @Param("status") RelationshipStatus status);
+
+    @Query("SELECT r FROM Relationship r WHERE r.pk.friend.id = :friendId AND r.relationshipStatus = :status")
+    List<Relationship> findAllRelationshipsByFriendIdAndStatus(@Param("friendId") Long friendId, @Param("status") RelationshipStatus status);
+
+    @Query("SELECT r FROM Relationship r WHERE r.pk.user.id = :userId AND r.pk.friend.id = :friendId")
+    Optional<Relationship> findRelationshipByUserIdAndFriendId(@Param("userId") Long userId, @Param("friendId") Long friendId);
+
+    @Query("SELECT r FROM Relationship r WHERE " +
+            "(r.pk.user.id = :userId AND r.pk.friend.id = :friendId) OR " +
+            "(r.pk.user.id = :friendId AND r.pk.friend.id = :userId)")
+    List<Relationship> findAllRelationships(@Param("userId") Long userId, @Param("friendId") Long friendId);
+
+}
