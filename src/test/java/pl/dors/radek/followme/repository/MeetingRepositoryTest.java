bcc84b45a160081f51a3db22c36d251fc9b67f8b
Radoslaw Dors <radoslaw.dors@comarch.pl>
2016-11-21 15:59:27 +0100

    MeetingRepositoryTest

--- src/test/java/pl/dors/radek/followme/repository/MeetingRepositoryTest.java
+++ src/test/java/pl/dors/radek/followme/repository/MeetingRepositoryTest.java
@@ -34,43 +34,84 @@ public class MeetingRepositoryTest {
     private MeetingRepository meetingRepository;
 
     private List<Meeting> meetings;
+    private List<User> users;
+
+    Meeting meeting1;
+    Meeting meeting2;
+    User user1;
+    User user2;
 
     @Before
     public void setUp() throws Exception {
-        User u1 = new User();
-        u1.setUsername("user1");
-        User u2 = new User();
-        u2.setUsername("user2");
+        user1 = new User();
+        user1.setUsername("user1");
+        user2 = new User();
+        user2.setUsername("user2");
 
-        List<User> users = Arrays.asList(u1, u2);
+        users = Arrays.asList(user1, user2);
         users.forEach(entityManager::persist);
 
         MeetingUser mu1 = new MeetingUser();
-        mu1.setUser(u1);
+        mu1.setUser(user1);
         mu1.setOwner(false);
         MeetingUser mu2 = new MeetingUser();
-        mu2.setUser(u2);
+        mu2.setUser(user2);
         mu2.setOwner(false);
         MeetingUser mu3 = new MeetingUser();
-        mu3.setUser(u2);
+        mu3.setUser(user2);
         mu3.setOwner(false);
 
-        Meeting m1 = new Meeting();
-        m1.setName("m111");
-        m1.getMeetingUsers().add(mu1);
-        m1.getMeetingUsers().add(mu2);
+        meeting1 = new Meeting();
+        meeting1.setName("m111");
+        meeting1.getMeetingUsers().add(mu1);
+        meeting1.getMeetingUsers().add(mu2);
 
-        mu1.setMeeting(m1);
-        mu2.setMeeting(m1);
+        mu1.setMeeting(meeting1);
+        mu2.setMeeting(meeting1);
 
-        Meeting m2 = new Meeting();
-        m2.setName("m222");
-        m2.getMeetingUsers().add(mu3);
+        meeting2 = new Meeting();
+        meeting2.setName("m222");
+        meeting2.getMeetingUsers().add(mu3);
+        meeting2.setActive(true);
 
-        mu3.setMeeting(m2);
+        mu3.setMeeting(meeting2);
 
-        meetings = Arrays.asList(m1, m2);
+        meetings = Arrays.asList(meeting1, meeting2);
         meetings.forEach(entityManager::persist);
+    }
+
+    @Test
+    public void findAll() throws Exception {
+        List<Meeting> result = meetingRepository.findAll();
+
+        assertThat(result).hasSize(2);
+        assertThat(result).containsOnlyElementsOf(meetings);
+    }
+
+    @Test
+    public void findAll_Empty() throws Exception {
+        meetings.forEach(entityManager::remove);
+
+        List<Meeting> result = meetingRepository.findAll();
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findByActiveTrue() throws Exception {
+        List<Meeting> result = meetingRepository.findByActiveTrue();
+
+        assertThat(result).hasSize(1);
+        assertThat(result).containsOnly(meeting2);
+    }
+
+    @Test
+    public void findByActiveTrue_Empty() throws Exception {
+        entityManager.remove(meeting2);
+
+        List<Meeting> result = meetingRepository.findByActiveTrue();
+
+        assertThat(result).isEmpty();
     }
 
     @Test
@@ -82,11 +123,47 @@ public class MeetingRepositoryTest {
     }
 
     @Test
-    public void findAll() throws Exception {
-        List<Meeting> result = meetingRepository.findAll();
+    public void findByUsername_NotExist() throws Exception {
+        List<Meeting> result = meetingRepository.findByUsername("user12");
 
-        assertThat(result).hasSize(2);
-        assertThat(result).containsOnlyElementsOf(meetings);
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findById() throws Exception {
+        List<Meeting> result = meetingRepository.findByUserId(user1.getId());
+
+        assertThat(result).hasSize(1);
+        assertThat(result).containsOnly(meeting1);
+    }
+
+    @Test
+    public void findById_NotExist() throws Exception {
+        List<Meeting> result = meetingRepository.findByUserId(users.stream().mapToLong(user -> user.getId()).sum());
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findByUserIdAndActiveTrue() throws Exception {
+        List<Meeting> result = meetingRepository.findByUserIdAndActiveTrue(user2.getId());
+
+        assertThat(result).hasSize(1);
+        assertThat(result).containsOnly(meeting2);
+    }
+
+    @Test
+    public void findByUserIdAndActiveTrue_IdNotExist() throws Exception {
+        List<Meeting> result = meetingRepository.findByUserIdAndActiveTrue(users.stream().mapToLong(user -> user.getId()).sum());
+
+        assertThat(result).isEmpty();
+    }
+
+    @Test
+    public void findByUserIdAndActiveTrue_UserExistButInactive() throws Exception {
+        List<Meeting> result = meetingRepository.findByUserIdAndActiveTrue(user1.getId());
+
+        assertThat(result).isEmpty();
     }
 
 }
--- src/test/java/pl/dors/radek/followme/repository/MeetingUserRepositoryTest.java
+++ src/test/java/pl/dors/radek/followme/repository/MeetingUserRepositoryTest.java
@@ -40,8 +40,8 @@ public class MeetingUserRepositoryTest {
     public void setUp() throws Exception {
         List<MeetingUser> meetingUsers = new ArrayList<>();
         meetings = Arrays.asList(
-                new Meeting("m1"),
-                new Meeting("m2")
+                new Meeting("meeting1"),
+                new Meeting("meeting2")
         );
 
         User u1 = new User();
@@ -54,25 +54,25 @@ public class MeetingUserRepositoryTest {
         users.forEach(entityManager::persist);
 
         MeetingUser meetingUser1 = new MeetingUser();
-        meetingUser1.setMeeting(meetings.stream().filter(m -> m.getName().equals("m1")).findAny().get());
+        meetingUser1.setMeeting(meetings.stream().filter(m -> m.getName().equals("meeting1")).findAny().get());
         meetingUser1.setUser(users.stream().filter(p -> p.getUsername().equals("User1")).findAny().get());
         meetingUser1.setOwner(true);
         meetingUsers.add(meetingUser1);
-        meetings.stream().filter(m -> m.getName().equals("m1")).forEach(m -> m.getMeetingUsers().add(meetingUser1));
+        meetings.stream().filter(m -> m.getName().equals("meeting1")).forEach(m -> m.getMeetingUsers().add(meetingUser1));
 
         MeetingUser meetingUser2 = new MeetingUser();
-        meetingUser2.setMeeting(meetings.stream().filter(m -> m.getName().equals("m1")).findAny().get());
+        meetingUser2.setMeeting(meetings.stream().filter(m -> m.getName().equals("meeting1")).findAny().get());
         meetingUser2.setUser(users.stream().filter(p -> p.getUsername().equals("User2")).findAny().get());
         meetingUser2.setOwner(false);
         meetingUsers.add(meetingUser2);
-        meetings.stream().filter(m -> m.getName().equals("m1")).forEach(m -> m.getMeetingUsers().add(meetingUser2));
+        meetings.stream().filter(m -> m.getName().equals("meeting1")).forEach(m -> m.getMeetingUsers().add(meetingUser2));
 
         MeetingUser meetingUser3 = new MeetingUser();
-        meetingUser3.setMeeting(meetings.stream().filter(m -> m.getName().equals("m2")).findAny().get());
+        meetingUser3.setMeeting(meetings.stream().filter(m -> m.getName().equals("meeting2")).findAny().get());
         meetingUser3.setUser(users.stream().filter(p -> p.getUsername().equals("User3")).findAny().get());
         meetingUser3.setOwner(false);
         meetingUsers.add(meetingUser3);
-        meetings.stream().filter(m -> m.getName().equals("m2")).forEach(m -> m.getMeetingUsers().add(meetingUser3));
+        meetings.stream().filter(m -> m.getName().equals("meeting2")).forEach(m -> m.getMeetingUsers().add(meetingUser3));
 
         meetings.forEach(entityManager::persist);
     }
@@ -88,7 +88,7 @@ public class MeetingUserRepositoryTest {
     @Test
     public void findByMeeting() throws Exception {
         List<MeetingUser> result = meetingUserRepository.findByMeetingId(
-                meetings.stream().filter(m -> m.getName().equals("m2")).findAny().get().getId());
+                meetings.stream().filter(m -> m.getName().equals("meeting2")).findAny().get().getId());
 
         assertThat(result).hasSize(1);
         assertThat(result.get(0).getUser().getUsername()).isEqualTo("User3");
